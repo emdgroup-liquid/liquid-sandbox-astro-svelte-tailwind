8621a2453d4abc3ee532213537efcf69767f8982
Boris Diakur <borisdiakur@gmail.com>
2022-10-26 15:52:56 +0200

    update liquid

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const liquidPreset = require("@emdgroup-liquid/liquid/dist/css/tailwind-preset.js");
+const liquidPreset = require("@emdgroup-liquid/liquid/dist/css/tailwind-preset.cjs");
 
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
